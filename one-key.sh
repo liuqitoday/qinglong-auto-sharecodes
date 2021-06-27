ca9eca19557442ffaf4f9c8837c6c3bf28a9a3b5
liuqitoday <liuqitoday@163.com>
2021-06-27 16:51:25 +0800

    fix: change path for code.sh

--- one-key.sh
+++ one-key.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 
 dir_shell=/ql/config
-code_shell_path=$dir_shell/code.sh
+dir_script=/ql/script
+code_shell_path=$dir_script/code.sh
 task_before_shell_path=$dir_shell/task_before.sh
 
 # 下载code.sh
 curl -s --connect-timeout 3 https://raw.githubusercontent.com/liuqitoday/qinglong-auto-sharecodes/master/code.sh > $code_shell_path
 
 # 判断是否下载成功
-if [ ! -f "$code_shell_path" ]; then
+if [ ! -a "$code_shell_path" ]; then
     echo "code.sh 下载失败"
     exit 0
 fi
@@ -16,10 +17,17 @@ fi
 # 授权
 chmod 755 $code_shell_path
 
-# 替换 code.sh 中的仓库名
-sed -i '' 's/chinnkarahoi/JDHelloWorld/g' $code_shell_path
+# 替换 code.sh 中的仓库作者名
+repoAuthor=${1:-'JDHelloWorld'}
+sed -i '' "s/chinnkarahoi/$repoAuthor/g" $code_shell_path
 
 # 将 code.sh 添加到定时任务
+if [ "$(grep -c code.sh $dir_shell/crontab.list)" = 0 ]; then
+    echo "开始添加 task code.sh"
+    # 获取token
+    token=$(cat $dir_shell/auth.json | jq .token)
+    curl -H 'Accept: application/json' -H "Authorization: Bearer $token" -H 'Content-Type: application/json;charset=UTF-8' -H 'Accept-Language: zh-CN,zh;q=0.9' --data-binary '{"name":"生成内部互助码","command":"task code.sh","schedule":"6 7 * * *"}' --compressed 'http://127.0.0.1:5700/api/crons?t=1624782068473'
+fi
 
 # 下载 task_before.sh
 curl -s --connect-timeout 3 https://raw.githubusercontent.com/liuqitoday/qinglong-auto-sharecodes/master/task_before.sh > $task_before_shell_path
