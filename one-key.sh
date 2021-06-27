f8f7e666f2fa6221ae6e9fc80cb32e961f940c0b
liuqitoday <liuqitoday@163.com>
2021-06-27 17:50:14 +0800

    fix: error msg

--- one-key.sh
+++ one-key.sh
@@ -32,7 +32,6 @@ if [ "$(grep -c code.sh /ql/config/crontab.list)" = 0 ]; then
     echo "开始添加 task code.sh"
     # 获取token
     token=$(cat /ql/config/auth.json | jq --raw-output .token)
-    sed -i 
     curl -s -H 'Accept: application/json' -H "Authorization: Bearer $token" -H 'Content-Type: application/json;charset=UTF-8' -H 'Accept-Language: zh-CN,zh;q=0.9' --data-binary '{"name":"生成内部互助码","command":"task code.sh","schedule":"6 7 * * *"}' --compressed 'http://127.0.0.1:5700/api/crons?t=1624782068473'
 fi
 
